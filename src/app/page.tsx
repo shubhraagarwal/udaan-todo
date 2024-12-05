0a675febf54fdb7a7d098673b6dbbd4c660cd57e
Shubhra Agarwal <agarwalshubhra2002@gmail.com>
2024-12-05 15:02:55 +0530

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,107 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import Check from "../../public/checkmark.png";
+import Delete from "../../public/delete.png";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [inputTodo, setInputTodo] = useState("");
+  const [todo, setTodo] = useState<[]>([]);
+  const [completedTask, setCompletedTask] = useState([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  function ondrop(task) {
+    setCompletedTask([...completedTask, task]);
+    setTodo(todo.filter((item) => item !== task));
+  }
+  return (
+    <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[#0D0714]">
+      <div className="flex bg-[#1D1825] flex-col  min-h-[80vh] w-1/2 h-full rounded-lg p-8 gap-10">
+        <div className="flex gap-4 items-center w-full">
+          <input
+            name="Add"
+            value={inputTodo}
+            onChange={(e) => setInputTodo(e.target.value)}
+            className="text-black bg-transparent border-2 border-[#9e78cf] px-2 py-1 rounded-lg focus-visible::outline-[#9e78cf] w-full"
+            placeholder="Add a new task"
+          />
+          <button
+            className="px-3 py-1 rounded-lg bg-[#9E78CF]"
+            onClick={() => {
+              setTodo([...todo, inputTodo]);
+              setInputTodo("");
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <p className="text-lg">+</p>
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <DragDropContext>
+          <div className="flex w-full flex-col  gap-5">
+            <p>Tasks to do - {todo.length}</p>
+            <ul className="flex flex-col gap-4">
+              {todo.map((task, index) => {
+                return (
+                  <Draggable
+                    key={index}
+                    type="todo"
+                    data={task}
+                  >
+                    <li className="flex items-center p-2 justify-between px-4 py-4 bg-[#15101C] w-full rounded-lg text-[#9e78cf]">
+                      {" "}
+                      {task}{" "}
+                      <div className="flex items-center gap-4">
+                        <Image
+                          src={Check}
+                          alt="checkmark"
+                          width={12}
+                          height={12}
+                          className="hover:cursor-pointer"
+                          onClick={() => {
+                            setCompletedTask([...completedTask, task]);
+                            setTodo(todo.filter((item) => item !== task));
+                          }}
+                        />
+                        <Image
+                          src={Delete}
+                          alt="delete"
+                          width={12}
+                          className="hover:cursor-pointer"
+                          height={12}
+                          onClick={() => {
+                            setTodo(todo.filter((item) => item !== task));
+                          }}
+                        />
+                      </div>
+                    </li>
+                  </Draggable>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="flex w-full flex-col gap-5">
+            <p>Done - {completedTask.length}</p>
+            <Droppable
+              types={["todo"]}
+              onDrop={ondrop}
+            >
+              <ul className="flex flex-col gap-4">
+                {completedTask.map((task, index) => {
+                  return (
+                    <li
+                      key={index}
+                      className="flex items-center p-2 justify-between px-4 py-4 bg-[#15101C] w-full rounded-lg text-[#78cfb0] line-through"
+                    >
+                      {" "}
+                      {task}
+                    </li>
+                  );
+                })}
+              </ul>
+            </Droppable>
+          </div>
+        </DragDropContext>
+      </div>
     </div>
   );
 }
